9b94881e5bc5aca63fdbc414cdef838edaa62a4a
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2024-04-26 18:12:58 +0200

    Starting button added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import "./index.css";
 
-function Test() {
-  return <h2>Test</h2>;
+function Title() {
+  return <h1 className="flex-centered">React Riddle</h1>;
+}
+
+function StartButton({ onStart, gameState }) {
+  return (
+    <button className={gameState ? "active" : ""} onClick={() => onStart()}>
+      {gameState ? "Finish the Quiz" : "Start the Quiz"}
+    </button>
+  );
 }
 
 function App() {
+  // Start the game
+  const [isStarted, setIsStarted] = useState(false);
+
+  const handleStarting = () => {
+    setIsStarted((previousState) => !previousState);
+
+    let gameState = !isStarted ? "is" : "isnt";
+
+    console.log(`Game ${gameState} started`);
+  };
+
   return (
-    <>
-      <Test />
-    </>
+    <div className="wrapper">
+      <Title />
+      <StartButton onStart={handleStarting} gameState={isStarted} />
+    </div>
   );
 }
 
